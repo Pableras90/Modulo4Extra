4486677c1885863c94bbd6f5e5db37afad4372bd
Pablo <p.reinaldo90@gmail.com>
2022-11-12 11:35:40 +0100

    3 commit

--- Reservas hotel/app.js
+++ Reservas hotel/app.js
@@ -1,21 +1,27 @@
 //100 Standar;120 Junior Suite;150 Suite.
-var totalPrice=0
+var totalPrice = 0
 
 function seleccionarHabitacion() {
     room = document.getElementById('roomType').value;
     return room;
 }
 
-function priceRoom() {
-    if (room === "Standard") {
+
+
+function typeRoom() {
+
+    if (seleccionarHabitacion() === "Standard") {
         priceRoom = 100;
-    } else if (room === "Junior Suite") {
+    } else if (seleccionarHabitacion() === "Junior Suite") {
         priceRoom = 120;
-    } else if (room === "Suite") {
+    } else if (seleccionarHabitacion() === "Suite") {
         priceRoom = 150;
     }
     return priceRoom;
-
 }
-document.getElementById("roomType").addEventListener("change", console.log(seleccionarHabitacion()));
-console.log(priceRoom())
+
+document.getElementById("roomType").addEventListener("change", typeRoom)
+
+console.log(typeRoom())
+
+
